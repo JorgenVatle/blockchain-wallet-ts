bf11ef9e3d4f6260ff98bcb67a1f48c79ecccf2f
Jørgen Vatle <Jorgen@WcAServices.net>
2019-11-18 16:12:10 +0200

    Assert that BlockchainWallet can fetch balances.

--- src/BlockchainWallet.test.ts
+++ src/BlockchainWallet.test.ts
@@ -3,8 +3,21 @@ import BlockchainApi from './BlockchainApi';
 import LocalConfig from './Interfaces/LocalConfig';
 
 describe('BlockchainWallet', () => {
-    const Wallet = new BlockchainApi({
-        apiKey: LocalConfig.blockchain.apiKey,
-        apiUrl: LocalConfig.blockchain.apiUrl,
-    })
+    let wallet: BlockchainWallet;
+
+    beforeAll(async () => {
+        wallet = await new BlockchainApi({
+            apiKey: LocalConfig.blockchain.apiKey,
+            apiUrl: LocalConfig.blockchain.apiUrl,
+        }).getWallet({
+            guid: LocalConfig.wallet.guid,
+            password: LocalConfig.wallet.password,
+        })
+    });
+
+    test('Can get balance', async () => {
+        const { balance } = await wallet.balance;
+        expect(balance).toBeDefined();
+        expect(balance).toBeGreaterThanOrEqual(0);
+    });
 });
