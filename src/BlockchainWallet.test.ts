10b0a90d9d7dd5440b569a6858bb352fa8b15a49
Jørgen Vatle <Jorgen@WcAServices.net>
2019-11-18 16:03:05 +0200

    Use LocalConfig for BlockchainWallet tests.

--- src/BlockchainWallet.test.ts
+++ src/BlockchainWallet.test.ts
@@ -1,15 +1,10 @@
-import Config from 'config';
 import BlockchainWallet from './BlockchainWallet';
 import BlockchainApi from './BlockchainApi';
-import { LocalConfig } from './Interfaces/LocalConfig';
+import LocalConfig from './Interfaces/LocalConfig';
 
 describe('BlockchainWallet', () => {
-    const config = {
-        blockchain: Config.get<LocalConfig.blockchain>('blockchain'),
-        wallet: Config.get<LocalConfig.wallet>('wallet'),
-    };
     const Wallet = new BlockchainApi({
-        apiKey: config.blockchain.apiKey,
-        apiUrl: config.blockchain.apiUrl,
+        apiKey: LocalConfig.blockchain.apiKey,
+        apiUrl: LocalConfig.blockchain.apiUrl,
     })
 });
