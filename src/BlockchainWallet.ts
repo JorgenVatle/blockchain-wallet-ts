f956da8498bef7d6a1ac41cd2cbc4c54a2b78217
Jørgen Vatle <Jorgen@WcAServices.net>
2019-11-18 12:52:52 +0200

    Add createWallet() method to BlockchainWallet.

--- src/BlockchainWallet.ts
+++ src/BlockchainWallet.ts
@@ -1,20 +1,5 @@
 import Axios, { AxiosInstance } from 'axios';
 
-interface BlockchainWalletConfig {
-    /**
-     * URL to running 'service-my-wallet', include port!
-     * E.g. http://localhost:3000
-     *
-     * @link https://github.com/blockchain/service-my-wallet-v3
-     */
-    apiUrl: string;
-
-    /**
-     * Your API Key
-     */
-    apiKey?: string;
-}
-
 export default class BlockchainWallet {
 
     /**
@@ -30,11 +15,100 @@ export default class BlockchainWallet {
     /**
      * Blockchain Wallet constructor
      */
-    public constructor(config: BlockchainWalletConfig) {
+    public constructor(config: BlockchainWallet.config) {
         this.apiKey = config.apiKey;
         this.http = Axios.create({
             baseURL: config.apiUrl,
         });
     }
 
+    /**
+     * Create a new wallet.
+     */
+    public createWallet(params: BlockchainWallet.walletParams) {
+        return this.http.get<BlockchainWallet.Response.createWallet>('/api/v2/create', {
+            params,
+        }).then(({ data }) => data);
+    }
+
+}
+
+export namespace BlockchainWallet {
+
+    /**
+     * Constructor options.
+     */
+    export interface config {
+        /**
+         * URL to running 'service-my-wallet', include port!
+         * E.g. http://localhost:3000
+         *
+         * @link https://github.com/blockchain/service-my-wallet-v3
+         */
+        apiUrl: string;
+
+        /**
+         * Your API Key
+         */
+        apiKey?: string;
+    }
+
+    /**
+     * Create wallet query parameters.
+     *
+     * @link https://github.com/blockchain/service-my-wallet-v3#creating-a-new-blockchain-wallet
+     */
+    export interface walletParams {
+        /**
+         * Main wallet password.
+         */
+        password: string;
+
+        /**
+         * Blockchain.com API key.
+         */
+        api_code?: string;
+
+        /**
+         * Private key to import into wallet as first address.
+         */
+        priv?: string;
+
+        /**
+         * Label to give the first address generated in the wallet.
+         */
+        label?: string;
+
+        /**
+         * Email address to associate with the newly created wallet.
+         */
+        email?: string;
+    }
+
+    /**
+     * API Responses.
+     */
+    export namespace Response {
+
+        /**
+         * Wallet create request response.
+         */
+        export interface createWallet {
+            /**
+             * Wallet ID.
+             */
+            guid: string;
+
+            /**
+             * Bitcoin wallet address.
+             */
+            address: string;
+
+            /**
+             * Label you associated with your wallet.
+             */
+            label?: string;
+        }
+
+    }
 }
