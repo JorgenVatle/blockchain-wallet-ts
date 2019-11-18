b540d6a4afe4b8361ced1e81ddc5b5b377dd9eb5
Jørgen Vatle <Jorgen@WcAServices.net>
2019-11-18 15:51:55 +0200

    Use LocalConfig module for setting config types.

--- src/BlockchainWallet.test.ts
+++ src/BlockchainWallet.test.ts
@@ -1,11 +1,12 @@
 import Config from 'config';
 import BlockchainWallet from './BlockchainWallet';
 import BlockchainApi from './BlockchainApi';
+import { LocalConfig } from './Interfaces/LocalConfig';
 
 describe('BlockchainWallet', () => {
     const config = {
-        blockchain: Config.get<{ apiKey: string, walletUrl: string }>('blockchain'),
-        wallet: Config.get<{ guid: string, password: string }>('wallet'),
+        blockchain: Config.get<LocalConfig.blockchain>('blockchain'),
+        wallet: Config.get<LocalConfig.wallet>('wallet'),
     };
     const Wallet = new BlockchainApi({
         apiKey: config.blockchain.apiKey,
---
+++ src/Interfaces/LocalConfig.ts
@@ -0,0 +1,10 @@
+export namespace LocalConfig {
+    export interface blockchain {
+        apiKey: string;
+        walletUrl: string;
+    }
+    export interface wallet {
+        guid: string;
+        password: string;
+    }
+}
